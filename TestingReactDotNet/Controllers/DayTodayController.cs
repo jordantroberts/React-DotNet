5c264bf93f3d77d211d70d2db67d307738d32d8b
Jordan Roberts <jordanroberts@talk21.com>
2019-08-20 08:29:05 +0100

    There was nothing async about the GetDate method so I removed the complexity of the async method.

--- TestingReactDotNet/Controllers/DayTodayController.cs
+++ TestingReactDotNet/Controllers/DayTodayController.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TestingReactDotNet.Controllers {
+
     [Route ("api/[controller]")]
     public class DayTodayController : Controller {
+
         [HttpGet, Route ("GetDate")]
-        public async Task<string> GetDate () {
+        public  string GetDate () {
+
             var info = $"Today is {DateTime.Now.ToString("dddd, dd MMMM yyyy")} and the time is {DateTime.Now.ToString("hh:mm tt")}";
             return info;
+
         }
     }
 }
