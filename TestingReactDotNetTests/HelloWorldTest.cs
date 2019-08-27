f250af9c8df3579e6d56d63aa92ab44a131c852c
Jordan Roberts <jordanroberts@talk21.com>
2019-08-27 09:42:09 +0100

    Added Test file for Hello World controller

---
+++ TestingReactDotNetTests/HelloWorldTest.cs
@@ -0,0 +1,20 @@
+using System.Web.Mvc;
+using NUnit.Framework;
+using TestingReactDotNet.Controllers;
+
+namespace TestingReactDotNetTests
+{
+    public class HelloWorldTests
+    {
+        [Test]
+        public void ReturnsStringHelloWorld()
+        {
+            var controller = new HelloWorldController();
+            string result = controller.Message();
+            string expected = "Hello World";
+            Assert.AreEqual(expected, result); 
+
+        }
+    }
+}
+
