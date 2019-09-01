793ed6a3b5a6a4c5222bdffb3f11ef40fdb9a125
Jordan Roberts <jordanroberts@talk21.com>
2019-09-01 18:01:02 +0100

    Added test for DisplayName Controller

---
+++ TestingReactDotNetTests/DisplayNameTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using TestingReactDotNet.Controllers;
+
+
+namespace TestingReactDotNetTests
+{
+    public class DisplayNameTest
+    {
+        [Test]
+        public void ReturnsHelloName()
+        {
+            var controller = new DisplayNameController();
+           
+            string result = controller.Greeting("Jordan");
+            string expected = "Hello, Jordan";
+            Assert.AreEqual(expected, result); 
+
+        }
+    }
+}
+
