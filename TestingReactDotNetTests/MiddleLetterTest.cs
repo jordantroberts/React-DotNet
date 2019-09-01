800f7f89e695b4f80e09af77fef7ee958804f0bf
Jordan Roberts <jordanroberts@talk21.com>
2019-09-01 18:02:50 +0100

    Unit test added and passed for even word

---
+++ TestingReactDotNetTests/MiddleLetterTest.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Web.Mvc;
+using NUnit.Framework;
+using TestingReactDotNet.Controllers;
+
+namespace TestingReactDotNetTests
+{
+    public class MiddleLetterTest
+    {
+        [Test]
+        public void ReturnsMiddleLetterOfEvenWord()
+        {
+            var controller = new MiddleLetterController();
+            string result = controller.GetMiddle("test");
+            string expected = "es";
+            Assert.AreEqual(expected, result); 
+        }
+    }
+}
+
+
