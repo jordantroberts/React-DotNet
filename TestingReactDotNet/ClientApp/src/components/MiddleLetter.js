cdc94b3cbccba794e0fe66a0a8a00552b8a30b31
Jordan Roberts <jordanroberts@talk21.com>
2019-09-01 17:55:34 +0100

    Tidied up code

--- TestingReactDotNet/ClientApp/src/components/MiddleLetter.js
+++ TestingReactDotNet/ClientApp/src/components/MiddleLetter.js
@@ -1,53 +1,53 @@
-﻿
-import React, { Component } from "react"; 
+﻿import React, { Component } from "react";
 
 export class MiddleLetter extends Component {
+  state = {
+    word: "",
+    greeting: ""
+  };
 
-    state = {
-        word: "",
-        greeting: ""
-    };
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    });
+  }
 
-    updateInput(key, value) {
-        this.setState({
-            [key]: value
-        });
-    }
-
-     calculate() {
-       
-    fetch("api/MiddleLetter/GetMiddle?word=" + encodeURIComponent(this.state.word))
+  calculate() {
+    fetch(
+      "api/MiddleLetter/GetMiddle?word=" + encodeURIComponent(this.state.word)
+    )
       .then(response => response.text())
       .then(data => {
         this.setState({ greeting: data });
       });
   }
 
-    render() {
-
-          if ("" !== this.state.greeting) {
-            return <h1>{this.state.greeting}</h1>;
-          } else {
-              return (
-            <center>
-                <div className="App">
-                    <div>
-                        Type a word...
-                        <br />
-                        <input
-                            type="text"
-                            placeholder="Type a word here..."
-                            value={this.state.word}
-                            onChange={e => this.updateInput("word", e.target.value)}
-                        />
-                        <button onClick={() => this.calculate()}>Submit</button>
-                        <br />
-                    </div>
-                </div>
-            </center >
-                );
-        }
+  render() {
+    if ("" !== this.state.greeting) {
+      return (
+        <center>
+          <h1>The middle letter/s of the word: "{this.state.greeting}"</h1>
+        </center>
+      );
+    } else {
+      return (
+        <center>
+          <div className="App">
+            <div>
+              Type a word and click Submit to see the middle letter or letters of the word: 
+              <br />
+              <input
+                type="text"
+                placeholder="Type a word here..."
+                value={this.state.word}
+                onChange={e => this.updateInput("word", e.target.value)}
+              />
+              <button onClick={() => this.calculate()}>Submit</button>
+              <br />
+            </div>
+          </div>
+        </center>
+      );
     }
+  }
 }
-
- 
